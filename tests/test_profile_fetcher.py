b040615d650f0f078f8270a8780238c3109f7f0a
Mauro Doglio <mdoglio@mozilla.com>
2017-03-17 18:27:27 +0000

    Use HBase to retrieve addons list

--- tests/test_profile_fetcher.py
+++ tests/test_profile_fetcher.py
@@ -1,11 +1,30 @@
 from taar.profile_fetcher import ProfileFetcher
+from taar import hbase_client
 
 
-def test_profile_fetcher_returns_dict():
+def get_client_addons_mock(*args, **kwargs):
+    return {
+        u'active_addons': [
+            {u'addon_id': u'e10srollout@mozilla.org'},
+            {u'addon_id': u'firefox@getpocket.com'}
+        ]
+    }
+
+
+def test_profile_fetcher_returns_dict(monkeypatch):
+    monkeypatch.setattr(hbase_client.HBaseClient,
+                        'get_client_addons',
+                        get_client_addons_mock)
+
+    monkeypatch.setattr(hbase_client.HBaseClient,
+                        '_get_master_address',
+                        lambda x: 'master-ip-address')
+
     fetcher = ProfileFetcher()
 
     fetcher.get("random-client-id") == {
         "installed_addons": [
-            "uBlock0@raymondhill.net",
+            "e10srollout@mozilla.org",
+            "firefox@getpocket.com"
         ],
     }
