7970fd65a7e66f1e07c92d0dcac990f80dc08745
Alessio Placitelli <alessio.placitelli@gmail.com>
2017-03-21 11:15:20 +0100

    Enable fetching locale data from HBase

--- tests/test_profile_fetcher.py
+++ tests/test_profile_fetcher.py
@@ -2,19 +2,20 @@ from taar.profile_fetcher import ProfileFetcher
 from taar import hbase_client
 
 
-def get_client_addons_mock(*args, **kwargs):
+def get_client_profile_mock(*args, **kwargs):
     return {
         u'active_addons': [
             {u'addon_id': u'e10srollout@mozilla.org'},
             {u'addon_id': u'firefox@getpocket.com'}
-        ]
+        ],
+        u'locale': 'it-IT'
     }
 
 
 def test_profile_fetcher_returns_dict(monkeypatch):
     monkeypatch.setattr(hbase_client.HBaseClient,
-                        'get_client_addons',
-                        get_client_addons_mock)
+                        'get_client_profile',
+                        get_client_profile_mock)
 
     monkeypatch.setattr(hbase_client.HBaseClient,
                         '_get_master_address',
@@ -27,4 +28,5 @@ def test_profile_fetcher_returns_dict(monkeypatch):
             "e10srollout@mozilla.org",
             "firefox@getpocket.com"
         ],
+        "locale": "it-IT"
     }
