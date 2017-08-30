07c9d64c8a9bc1498a1b2debc0ae5d1f86b635c2
Mauro Doglio <mdoglio@mozilla.com>
2017-08-30 18:27:59 +0100

    Add docstrings to RecommendationManager

--- taar/recommenders/recommendation_manager.py
+++ taar/recommenders/recommendation_manager.py
@@ -7,19 +7,20 @@ from ..profile_fetcher import ProfileFetcher
 logger = logging.getLogger(__name__)
 
 
-class RecommendationManager:
+class RecommendationManager(object):
+    """A class representing a collection of recommenders."""
 
     def __init__(self, profile_fetcher=None, recommenders=None):
-        # Instantiate the object to get the client info.
+        """Initialize the user profile fetcher and the recommenders.
+
+        Note: The order of the recommenders matters!
+        """
         if profile_fetcher is None:
             logger.info("Initializing profile_fetcher")
             self.profile_fetcher = ProfileFetcher()
         else:
             self.profile_fetcher = profile_fetcher
 
-        # Init the recommenders. Note: ORDER MATTERS!
-        # The recommendation logic will go through each recommender and pick the
-        # first one that "can_recommend".
         if not recommenders:
             logger.info("Initializing recommenders")
             self.recommenders = (
@@ -30,6 +31,11 @@ class RecommendationManager:
             self.recommenders = recommenders
 
     def recommend(self, client_id, limit):
+        """Return recommendations for the given client.
+
+        The recommendation logic will go through each recommender and pick the
+        first one that "can_recommend".
+        """
         # Get the info for the requested client id.
         client_info = self.profile_fetcher.get(client_id)
         if client_info is None:
