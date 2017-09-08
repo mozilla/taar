5de31662bb4bbe4fee6ff47297aba162267f1d35
mlopatka <mlopatka>
2017-09-08 17:33:19 +0200

    Add a similarity-recommender model to TAAR

---
+++ taar/recommenders/similarity_recommender.py
@@ -0,0 +1,154 @@
+import logging
+from ..recommenders import utils
+from .base_recommender import BaseRecommender
+from scipy.spatial import distance
+
+S3_BUCKET = 'telemetry-private-analysis-2'
+DONOR_LIST_KEY = 'taar/legacy/addon_donors.json'
+LR_CURVES_SIMILARITY_TO_PROBABILITY = 'taar/legacy/test/lr_curves.json'
+
+CATEGORICAL_FEATURES = ["geoCity", "locale", "os"]
+CONTINUOUS_FEATURES = ["subsessionLength", "bookmark_count", "tab_open_count", "total_uri", "unique_tlds"]
+
+logger = logging.getLogger(__name__)
+
+
+class SimilarityRecommender(BaseRecommender):
+    """ A recommender class that returns top N addons based on the client similarity
+    with a set of candidate addon donors. Several telemetry fields are used to compute
+    pairwise similarity with the donors and similarities are converted into a likelihood
+    ratio of being a good match versus not being a good match. These quantities are then
+    used to rank specific addons for recommendation.
+
+    This will load a json file containing updated list of addon donors updated periodically
+    by a separate weekly process using Longitdudinal Telemetry data.
+
+    This recommender may provide useful recommendations when collaborative_recommender
+    may not work.
+    """
+
+    def __init__(self):
+        self.donors_list = []
+
+        # Download the addon donors list.
+        donors_pool = utils.get_s3_json_content(S3_BUCKET, DONOR_LIST_KEY)
+        if donors_pool is None:
+            logger.error("Cannot download the donor list: {}".format(DONOR_LIST_KEY))
+
+        for donor in donors_pool:
+            # Separate out the categorical and numerical features for the donor.
+            d = {
+                'categorical_features': [donor.get(specified_key) for specified_key in CATEGORICAL_FEATURES],
+                'continuous_features': [donor.get(specified_key) for specified_key in CONTINUOUS_FEATURES],
+                'active_addons': donor['activeAddons']
+            }
+            self.donors_list.append(d)
+
+        # Download the probability mapping curves from similarity to likelihood of being a good donor.
+        self.lr_curves = utils.get_s3_json_content(S3_BUCKET, LR_CURVES_SIMILARITY_TO_PROBABILITY)
+        if self.lr_curves is None:
+            logger.error("Cannot download the lr curves: {}".format(LR_CURVES_SIMILARITY_TO_PROBABILITY))
+
+    def can_recommend(self, client_data):
+        # We can't recommend if we don't have our data files.
+        if self.donors_list is None or self.lr_curves is None:
+            return False
+
+        # Check that the client info contains a non-None value for each required telemetry field.
+        REQUIRED_FIELDS = ["geoCity", "subsessionLength", "locale", "os", "bookmark_count", "tab_open_count",
+                           "total_uri", "unique_tlds"]
+
+        has_fields = all([client_data.get(f, None) is not None for f in REQUIRED_FIELDS])
+        if not has_fields:
+            # Can not add extra info because client_id may not be available.
+            logger.error("Unusable client data encountered")
+        return has_fields
+
+    def get_lr(self, score):
+        """Compute a :float: likelihood ratio from a provided similarity score when compared
+        to two probability density functions which are computed and pre-loaded during init.
+
+        The numerator indicates the probability density that a particular similarity score
+        corresponds to a 'good' addon donor, i.e. a client that is similar in the sense of
+        telemetry variables. The denominator indicates the probability density that a particular
+        similarity score corresponds to a 'poor' addon donor
+
+        :param score: A similarity score between a pair of objects.
+        :returns: The approximate float likelihood ratio corresponding to provided score.
+        """
+        index = [abs(score - s[0]) for s in self.lr_curves]
+
+        # Find the index of the closest value that was precomputed in lr_curves
+        _, idx = min((val, idx) for (idx, val) in enumerate(index))
+
+        numer_val = self.lr_curves[idx][1][0]
+        denum_val = self.lr_curves[idx][1][1]
+
+        # Compute LR based on numerator and denominator values
+        return float(numer_val) / float(denum_val)
+
+    def get_similar_donors(self, client_data):
+        """Computes a set of :float: similarity scores between a client and a set of candidate
+        donors for which comparable variables have been measured.
+
+        A custom similarity metric is defined in this function that combines the Hamming distance
+        for categorical variables with the Canberra distance for continuous variables into a
+        univariate similarity metric between the client and a set of candidate donors loaded during
+        init.
+
+        :param client_data: a client data payload including a subset fo telemetry fields.
+        :rtype: :list:`tuples`: the approximate likelihood ratio corresponding to the
+        internally computed similarity score and a list of addons taken from that donor
+        """
+        client_categorical_feats = [client_data.get(specified_key) for specified_key in CATEGORICAL_FEATURES]
+        client_continuous_feats = [client_data.get(specified_key) for specified_key in CONTINUOUS_FEATURES]
+
+        donor_set = []
+
+        # This could be optimized by using cdist, however both logging and debugging benefit from having it in a loop.
+        # If performance improvements are necessary in he future, this is a good place to start.
+        for donor_index, donor in enumerate(self.donors_list):
+            # Compute the similarity score between the donor and the client in the space of categorical features.
+            try:
+                # Here a larger distance indicates a poorer match between categorical variables.
+                j_d = (distance.hamming(client_categorical_feats, donor['categorical_features']))
+            except ValueError:
+                logger.error("Unable to compute similarity over categorical features.",
+                             extra={"client_id": client_data["client_id"]})
+            # Compute the similarity score between the donor and the client in the space of numerical features.
+            try:
+                # Here a value close to zero indicates a good match in continuous variables.
+                j_c = (distance.canberra(client_continuous_feats, donor['continuous_features']))
+            except ValueError:
+                logger.error("Unable to compute similarity over continuous features.",
+                             extra={"client_id": client_data["client_id"]})
+
+            # Take the product of similarities to attain a univariate similarity score.
+            # Addition of 0.001 to j_c avoids a zero value from the categorical variables, allowing j_d precedence
+            single_score = abs((j_c + 0.001) * j_d)
+
+            # compute the LR based on precomputed distributions that relate the score
+            #  to a probability of providing good addon recommendations
+            lr_from_score = self.get_lr(single_score)
+            donor_set.append((lr_from_score, donor_index))
+            donor_index += 1
+
+        return sorted(donor_set, reverse=True)
+
+    def recommend(self, client_data, limit=10):
+        donor_set_ranking = self.get_similar_donors(client_data)
+        # 2.0 corresponds to a likelihood ratio of 2 meaning that donors are less than twice
+        # as likely to be 'good'. A value > 1.0 is sufficient, but we like this to be high.
+        if donor_set_ranking[0][0] < 2.0:
+            logger.warning("Addons recommended with very low similarity score, perhaps donor set is unrepresentative",
+                           extra={"maximum_similarity": donor_set_ranking[0]})
+
+        recommendations = []
+        for donor_ranking in donor_set_ranking:
+            # This expects a list of lists, if singletons are included, they should single elements lists.
+            if donor_ranking[0] > 1.0:
+                # Retrieve the index of the highest ranked donors and then append their installed addons
+                recommendations.extend(self.donors_list[donor_ranking[1]]['active_addons'])
+            if len(recommendations) > limit:
+                break
+        return recommendations[:limit]
---
+++ tests/test_similarityrecommender.py
@@ -0,0 +1,180 @@
+import boto3
+import json
+import numpy as np
+import pytest
+import scipy.stats
+from moto import mock_s3
+from taar.recommenders.similarity_recommender import \
+    SimilarityRecommender, CATEGORICAL_FEATURES, CONTINUOUS_FEATURES, S3_BUCKET
+
+
+FAKE_DONOR_DATA = [
+    {
+        "activeAddons": ["{test-guid-1}", "{test-guid-2}", "{test-guid-3}", "{test-guid-4}"],
+        "geoCity": "nowhere-us",
+        "subsessionLength": 1300,
+        "locale": "en-US",
+        "os": "mac",
+        "bookmark_count": 7,
+        "tab_open_count": 32,
+        "total_uri": 43,
+        "unique_tlds": 500
+    },
+    {
+        "activeAddons": ["{test-guid-5}", "{test-guid-6}", "{test-guid-7}", "{test-guid-8}"],
+        "geoCity": "pompei-it",
+        "subsessionLength": 67832,
+        "locale": "it-IT",
+        "os": "Linux",
+        "bookmark_count": 8166,
+        "tab_open_count": 232,
+        "total_uri": 42342,
+        "unique_tlds": 1203
+    },
+    {
+        "activeAddons": ["{test-guid-9}", "{test-guid-10}", "{test-guid-11}", "{test-guid-12}"],
+        "geoCity": "brasilia-br",
+        "subsessionLength": 5411,
+        "locale": "br-PT",
+        "os": "windows",
+        "bookmark_count": 17,
+        "tab_open_count": 3,
+        "total_uri": 432,
+        "unique_tlds": 10
+    }
+]
+
+
+def generate_fake_lr_curves(num_elements):
+    lr_index = list(np.linspace(0, 10, num_elements))
+    numerator_density = [scipy.stats.norm.pdf(float(i), 0.5, 0.5) for i in lr_index]
+    denominator_density = [scipy.stats.norm.pdf(float(i), 5.0, 1.5) for i in lr_index]
+    return list(zip(lr_index, zip(numerator_density, denominator_density)))
+
+
+def generate_a_fake_taar_client():
+    return {
+        "client_id": "test-client-001",
+        "activeAddons": [],
+        "geoCity": "rio-br",
+        "subsessionLength": 4911,
+        "locale": "br-PT",
+        "os": "windows",
+        "bookmark_count": 57,
+        "tab_open_count": 4,
+        "total_uri": 222,
+        "unique_tlds": 21
+    }
+
+
+@pytest.fixture
+def instantiate_mocked_s3_bucket():
+    mock_s3().start()
+
+    conn = boto3.resource('s3', region_name='us-west-2')
+    conn.create_bucket(Bucket=S3_BUCKET)
+    # Write the fake addon donor data to the mocked S3.
+    conn.Object(S3_BUCKET, key='taar/legacy/addon_donors.json').put(Body=json.dumps(FAKE_DONOR_DATA))
+    # Write the fake lr curves data to the mocked S3.
+    fake_lrs = generate_fake_lr_curves(1000)
+    conn.Object(S3_BUCKET, key='taar/legacy/test/lr_curves.json').put(Body=json.dumps(fake_lrs))
+
+    yield conn
+    mock_s3().stop()
+
+
+def test_can_recommend(instantiate_mocked_s3_bucket):
+    # Create a new instance of a SimilarityRecommender.
+    r = SimilarityRecommender()
+
+    # Test that we can't recommend if we have not enough client info.
+    assert not r.can_recommend({})
+
+    # Test that we can recommend for a normal client.
+    assert r.can_recommend(generate_a_fake_taar_client())
+
+    # Check that we can not recommend if any required client field is missing.
+    required_fields = CATEGORICAL_FEATURES + CONTINUOUS_FEATURES
+
+    for required_field in required_fields:
+        profile_without_x = generate_a_fake_taar_client()
+
+        # Make an empty value in a required field in the client info dict.
+        profile_without_x[required_field] = None
+        assert not r.can_recommend(profile_without_x)
+
+        # Completely remove (in place) the entire required field from the dict.
+        del profile_without_x[required_field]
+        assert not r.can_recommend(profile_without_x)
+
+
+def test_recommendations(instantiate_mocked_s3_bucket):
+    # Create a new instance of a SimilarityRecommender.
+    r = SimilarityRecommender()
+
+    recommendations = r.recommend(generate_a_fake_taar_client(), 10)
+
+    # Make sure the structure of the recommendations is correct and that we recommended the the right addons.
+    assert isinstance(recommendations, list)
+
+    # Make sure that the reported addons are the expected ones from the most similar donor.
+    assert "{test-guid-9}" in recommendations
+    assert "{test-guid-10}" in recommendations
+    assert "{test-guid-11}" in recommendations
+    assert "{test-guid-12}" in recommendations
+    assert len(recommendations) == 4
+
+
+def test_recommender_str(instantiate_mocked_s3_bucket):
+    # Tests that the string representation of the recommender is correct.
+    r = SimilarityRecommender()
+    assert str(r) == "SimilarityRecommender"
+
+
+def test_get_lr(instantiate_mocked_s3_bucket):
+    # Tests that the likelihood ratio values are not empty for extreme values and are realistic.
+    r = SimilarityRecommender()
+    assert r.get_lr(0.0001) is not None
+    assert r.get_lr(10.0) is not None
+    assert r.get_lr(0.001) > r.get_lr(5.0)
+
+
+def test_distance_functions(instantiate_mocked_s3_bucket):
+    # Tests the similarity functions via expected output when passing modified client data.
+    r = SimilarityRecommender()
+
+    # Generate a fake client.
+    test_client = generate_a_fake_taar_client()
+    recs = r.recommend(test_client)
+    assert len(recs) > 0
+
+    # Make it a generally poor match for the donors.
+    test_client.update({'total_uri': 10, 'bookmark_count': 2, 'subsessionLength': 10})
+
+    all_client_values_zero = test_client
+    # Make all categorical variables non-matching with any donor.
+    all_client_values_zero.update({key: 'zero' for key in test_client.keys() if key in CATEGORICAL_FEATURES})
+    recs = r.recommend(all_client_values_zero)
+    assert len(recs) == 0
+
+    # Make all continuous variables equal to zero.
+    all_client_values_zero.update({key: 0 for key in test_client.keys() if key in CONTINUOUS_FEATURES})
+    recs = r.recommend(all_client_values_zero)
+    assert len(recs) == 0
+
+    # Make all categorical variables non-matching with any donor.
+    all_client_values_high = test_client
+    all_client_values_high.update({key: 'one billion' for key in test_client.keys() if key in CATEGORICAL_FEATURES})
+    recs = r.recommend(all_client_values_high)
+    assert len(recs) == 0
+
+    # Make all continuous variables equal to a very high numerical value.
+    all_client_values_high.update({key: 1e60 for key in test_client.keys() if key in CONTINUOUS_FEATURES})
+    recs = r.recommend(all_client_values_high)
+    assert len(recs) == 0
+
+    # Test for 0.0 values if j_c is not normalized and j_d is fine.
+    j_c = 0.0
+    j_d = 0.42
+    assert abs(j_c * j_d) == 0.0
+    assert abs((j_c + 0.01) * j_d) != 0.0
