ab5a18ed8972a7d3974edfea4073d7b60379b791
Victor Ng <victor@crankycoder.com>
2017-11-29 12:32:57 -0500

    Updated LegacyRecommender to return (addon_id, weight) 2-tuples.

--- tests/test_legacyrecommender.py
+++ tests/test_legacyrecommender.py
@@ -4,17 +4,19 @@ from taar.recommenders import LegacyRecommender
 
 FAKE_LEGACY_DATA = {
     "guid-01":
-        ["guid-02"],
+        ["guid-02", 'guid-11-4'],
     "guid-03":
-        ["guid-05"],
+        ["guid-05", 'guid-16-4'],
     "guid-05":
-        ["guid-06"],
-    "guid-07": ["guid-08-1", "guid-09-2", "guid-10-3", "guid-11-4"],
-    "guid-12": ["guid-13-1", "guid-14-2", "guid-15-3", "guid-16-4",
-                "guid-17-5", "guid-18-6", "guid-19-7", "guid-20-8",
+        ["guid-06", 'guid-20-8'],
+    "guid-07": ["guid-08-1", "guid-09-2", "guid-10-3"],
+    "guid-12": ["guid-13-1", "guid-14-2", "guid-15-3",
+                "guid-17-5", "guid-18-6", "guid-19-7",
                 "guid-21-9", "guid-22-10"],
     "guid-23": []
 }
+
+LIMIT = 10
 
 
 @pytest.fixture
@@ -46,23 +48,6 @@ def test_recommendations(mock_s3_json_downloader):
     # Test that the legacy recommender returns the correct addons from the json loaded.
     r = LegacyRecommender()
 
-    limit = 10
-    profile_with_legacy = dict(
-        client_id="test-client-id",
-        disabled_addons_ids=["guid-01",
-                             "guid-05"],
-        locale="it-IT"
-    )
-
-    recommendations = r.recommend(profile_with_legacy, limit)
-
-    # Make sure the structure of the recommendations is correct and that we recommended the the right addons.
-    assert isinstance(recommendations, list)
-
-    # Make sure that the reported addons are the ones from the fake data.
-    assert "guid-02" in recommendations
-    assert "guid-06" in recommendations
-
     profile_with_many_legacy = dict(
         client_id="test-client-id",
         disabled_addons_ids=["guid-01",
@@ -71,17 +56,15 @@ def test_recommendations(mock_s3_json_downloader):
         locale="it-IT"
     )
 
-    recommendations = r.recommend(profile_with_many_legacy, limit)
-    assert len(recommendations) <= limit
-    assert "guid-13-1" in recommendations
-    assert "guid-22-10" not in recommendations
+    recommendations = r.recommend(profile_with_many_legacy, LIMIT)
+    assert len(recommendations) == LIMIT
+    assert ("guid-13-1", 1) in recommendations
+    assert ("guid-21-9", 1) not in recommendations
+    assert ("guid-22-10", 1) not in recommendations
+    assert ("guid-21-9", 1) not in recommendations
 
 
 def test_recommender_str(mock_s3_json_downloader):
     # Tests that the string representation of the recommender is correct
     r = LegacyRecommender()
     assert str(r) == "LegacyRecommender"
-
-
-def test_recommendation_weights(mock_s3_json_downloader):
-    raise NotImplementedError
