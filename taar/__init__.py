7e5a0432d96c27db5ae1caa811430da9f5a98309
Victor Ng <victor@crankycoder.com>
2018-05-16 10:14:32 -0400

    Adds a __version__ attribute to taar

--- taar/__init__.py
+++ taar/__init__.py
@@ -1,2 +1,5 @@
 from .profile_fetcher import ProfileFetcher     # noqa
 from .adapters.dynamo import ProfileController  # noqa
+import pkg_resources
+
+__version__ = pkg_resources.require("mozilla-taar3")[0].version
