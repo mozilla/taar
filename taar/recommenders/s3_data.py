ad0fee259a17352831dbbc1f265ebedc7b3fb9a4
Victor Ng <victor@crankycoder.com>
2018-07-27 14:16:17 -0400

    Added code to fetch the curated whitelist and generate a randomized list of 4 curated addons

--- taar/recommenders/s3_data.py
+++ taar/recommenders/s3_data.py
@@ -3,20 +3,45 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from .utils import get_s3_json_content
+import threading
+import random
 
 S3_BUCKET = 'telemetry-parquet'
 CURATED_WHITELIST = 'telemetry-ml/addon_recommender/top_200_whitelist.json'
 
 
-class WhitelistCache:
+class CuratedWhitelistCache:
+    """
+    This fetches the curated whitelist from S3.
+
+    A sample of the whitelist below :
+
+        [{'GUID': guid_string,
+          'Extension': extension_name,
+          'Copy (final)': english_description},
+        ]
+    """
     def __init__(self, ctx):
         self._ctx = ctx
+        self._lock = threading.RLock()
 
         # Enable this check when we start using srgutils
         # assert 'cache' in self._ctx
 
-    def get_curated_whitelist(self):
-        # TODO: replace this with the context version so we can inject
-        # S3 content easily
-        json_data = get_s3_json_content(S3_BUCKET, CURATED_WHITELIST)
-        return json_data
+        self._json_data = None
+
+    def get_whitelist(self):
+        with self._lock:
+            # TODO: replace this with the LazyJSONLoader from TAARlite for
+            # better performance and expiring dataset
+            if self._json_data is not None:
+                return self._json_data
+            self._json_data = get_s3_json_content(S3_BUCKET, CURATED_WHITELIST)
+            return self._json_data
+
+    def get_randomized_guid_sample(self, item_count):
+        """ Fetch a subset of randomzied GUIDs from the whitelist """
+        dataset = self.get_whitelist()
+        random.shuffle(dataset)
+        samples = dataset[:item_count]
+        return [s['GUID'] for s in samples]
