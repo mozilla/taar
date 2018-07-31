917c7b0b05b5e3ee4352c4cc8158516422b153fd
Victor Ng <victor@crankycoder.com>
2018-07-31 18:49:58 -0400

    Init checkin of hybrid recommendation engine and curated recommender

--- taar/recommenders/__init__.py
+++ taar/recommenders/__init__.py
@@ -1,13 +1,11 @@
 from .collaborative_recommender import CollaborativeRecommender
 from .locale_recommender import LocaleRecommender
-from .legacy_recommender import LegacyRecommender
 from .similarity_recommender import SimilarityRecommender
 from .recommendation_manager import RecommendationManager, RecommenderFactory
 
 
 __all__ = [
     'CollaborativeRecommender',
-    'LegacyRecommender',
     'LocaleRecommender',
     'SimilarityRecommender',
     'RecommendationManager',
