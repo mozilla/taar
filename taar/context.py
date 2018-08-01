d6e61c7813c6eea32af0b33b961071e1381439ee
Victor Ng <victor@crankycoder.com>
2018-08-01 11:16:17 -0400

    Refactor out the taar.context and replace with srgutil.context
    Removed testcases for context which are covered in srgutil

--- taar/context.py
+++ taar/context.py
@@ -14,56 +14,7 @@ chain.
 """
 
 from taar.recommenders import utils
-
-
-class Context:
-    def __init__(self, delegate=None):
-        if delegate is None:
-            delegate = {}
-
-        self._local_dict = {}
-        self._delegate = delegate
-
-    def __contains__(self, key):
-        try:
-            self[key]
-            return True
-        except KeyError:
-            return False
-
-    def __getitem__(self, key):
-        # This is a little tricky, we want to lookup items in our
-        # local namespace before we hit the delegate
-        try:
-            result = self._local_dict[key]
-        except KeyError:
-            result = self._delegate[key]
-        return result
-
-    def get(self, key, default):
-        try:
-            result = self[key]
-        except KeyError:
-            result = default
-        return result
-
-    def __setitem__(self, key, value):
-        self._local_dict[key] = value
-
-    def __delitem__(self, key):
-        del self._local_dict[key]
-
-    def wrap(self, ctx):
-        ctx_child = ctx.child()
-        this_child = self.child()
-        this_child._delegate = ctx_child
-        return this_child
-
-    def child(self):
-        """ In general, you should call this immediately in any
-        constructor that receives a context """
-
-        return Context(self)
+from srgutil.context import Context
 
 
 def default_context():
