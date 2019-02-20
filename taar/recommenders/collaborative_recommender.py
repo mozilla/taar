3f9ad80acfbf781b1e405269072f5c598fea5c19
Victor Ng <victor@crankycoder.com>
2019-02-20 13:08:20 -0500

    More logging and safety around the recommend method for each recommender
    I've added an exception handler that forces reload of underlying model in the case of recommendation error.
    Extra exception logging occurs if any recommender crashes

--- taar/recommenders/collaborative_recommender.py
+++ taar/recommenders/collaborative_recommender.py
@@ -118,54 +118,71 @@ class CollaborativeRecommender(AbstractRecommender):
 
         return False
 
+    def _recommend(self, client_data, limit, extra_data):
+        installed_addons_as_hashes = [
+            positive_hash(addon_id)
+            for addon_id in client_data.get("installed_addons", [])
+        ]
+
+        # Build the query vector by setting the position of the queried addons to 1.0
+        # and the other to 0.0.
+        query_vector = np.array(
+            [
+                1.0 if (entry.get("id") in installed_addons_as_hashes) else 0.0
+                for entry in self.raw_item_matrix
+            ]
+        )
+
+        # Build the user factors matrix.
+        user_factors = np.matmul(query_vector, self.model)
+        user_factors_transposed = np.transpose(user_factors)
+
+        # Compute the distance between the user and all the addons in the latent
+        # space.
+        distances = {}
+        for addon in self.raw_item_matrix:
+            # We don't really need to show the items we requested.
+            # They will always end up with the greatest score. Also
+            # filter out legacy addons from the suggestions.
+            hashed_id = addon.get("id")
+            str_hashed_id = str(hashed_id)
+            if (
+                hashed_id in installed_addons_as_hashes
+                or str_hashed_id not in self.addon_mapping
+                or self.addon_mapping[str_hashed_id].get("isWebextension", False)
+                is False
+            ):
+                continue
+
+            dist = np.dot(user_factors_transposed, addon.get("features"))
+            # Read the addon ids from the "addon_mapping" looking it
+            # up by 'id' (which is an hashed value).
+            addon_id = self.addon_mapping[str_hashed_id].get("id")
+            distances[addon_id] = dist
+
+        # Sort the suggested addons by their score and return the
+        # sorted list of addon ids.
+        sorted_dists = sorted(distances.items(), key=op.itemgetter(1), reverse=True)
+        recommendations = [(s[0], s[1]) for s in sorted_dists[:limit]]
+        return recommendations
+
     def recommend(self, client_data, limit, extra_data={}):
         # Addons identifiers are stored as positive hash values within the model.
         with self._lock:
-            installed_addons_as_hashes = [
-                positive_hash(addon_id)
-                for addon_id in client_data.get("installed_addons", [])
-            ]
+            try:
+                recommendations = self._recommend(client_data, limit, extra_data)
+            except Exception as e:
+                recommendations = []
 
-            # Build the query vector by setting the position of the queried addons to 1.0
-            # and the other to 0.0.
-            query_vector = np.array(
-                [
-                    1.0 if (entry.get("id") in installed_addons_as_hashes) else 0.0
-                    for entry in self.raw_item_matrix
-                ]
-            )
+                self._addon_mapping.force_expiry()
+                self._raw_item_matrix.force_expiry()
 
-            # Build the user factors matrix.
-            user_factors = np.matmul(query_vector, self.model)
-            user_factors_transposed = np.transpose(user_factors)
-
-            # Compute the distance between the user and all the addons in the latent
-            # space.
-            distances = {}
-            for addon in self.raw_item_matrix:
-                # We don't really need to show the items we requested.
-                # They will always end up with the greatest score. Also
-                # filter out legacy addons from the suggestions.
-                hashed_id = addon.get("id")
-                str_hashed_id = str(hashed_id)
-                if (
-                    hashed_id in installed_addons_as_hashes
-                    or str_hashed_id not in self.addon_mapping
-                    or self.addon_mapping[str_hashed_id].get("isWebextension", False)
-                    is False
-                ):
-                    continue
-
-                dist = np.dot(user_factors_transposed, addon.get("features"))
-                # Read the addon ids from the "addon_mapping" looking it
-                # up by 'id' (which is an hashed value).
-                addon_id = self.addon_mapping[str_hashed_id].get("id")
-                distances[addon_id] = dist
-
-            # Sort the suggested addons by their score and return the
-            # sorted list of addon ids.
-            sorted_dists = sorted(distances.items(), key=op.itemgetter(1), reverse=True)
-            recommendations = [(s[0], s[1]) for s in sorted_dists[:limit]]
+                self.logger.exception(
+                    "Collaborative recommender crashed for {}".format(
+                        client_data.get("client_id", "no-client-id")
+                    ),
+                    e,
+                )
 
         log_data = (client_data["client_id"], str([r[0] for r in recommendations]))
         self.logger.info(
