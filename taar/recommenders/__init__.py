ca3e44f01ad20fb2b36fc61eee0bc7bb4a32d4df
Victor Ng <victor@crankycoder.com>
2019-09-12 12:25:03 -0400

    Moved test client IDs into a dedicated fixtures.py module to simplify the call flow through the recommendation manager and ensemble recommender.

--- taar/recommenders/__init__.py
+++ taar/recommenders/__init__.py
@@ -2,12 +2,16 @@ from .collaborative_recommender import CollaborativeRecommender
 from .locale_recommender import LocaleRecommender
 from .similarity_recommender import SimilarityRecommender
 from .recommendation_manager import RecommendationManager, RecommenderFactory
+from .fixtures import TEST_CLIENT_IDS, EMPTY_TEST_CLIENT_IDS, hasher  # noqa
 
 
 __all__ = [
-    'CollaborativeRecommender',
-    'LocaleRecommender',
-    'SimilarityRecommender',
-    'RecommendationManager',
-    'RecommenderFactory',
+    "CollaborativeRecommender",
+    "LocaleRecommender",
+    "SimilarityRecommender",
+    "RecommendationManager",
+    "RecommenderFactory",
+    "TEST_CLIENT_IDS",
+    "EMPTY_TEST_CLIENT_IDS",
+    "hasher",
 ]
