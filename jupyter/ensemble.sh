dd904f232daf66459bbeb3f350112a94a393e276
Victor Ng <victor@crankycoder.com>
2020-03-09 16:24:28 -0400

    added 2 local SSDs to all nodes

--- jupyter/ensemble.sh
+++ jupyter/ensemble.sh
@@ -4,13 +4,13 @@ AWS_ACCESS_KEY_ID=AKIA5PV4VQIY4HJLNP4B
 AWS_SECRET_ACCESS_KEY=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
 requirements="mozilla-taar3==0.4.8 mozilla-srgutil==0.2.1 python-decouple==3.1 click==7.0 boto3==1.7.71 dockerflow==2018.4.0"
 
-gcloud beta dataproc clusters create jupyter-ensemble-broken-${RANDOM} \
+gcloud beta dataproc clusters create jupyter-ensemble-${RANDOM} \
     --bucket jupyter-ensemble \
     --enable-component-gateway \
     --image-version=1.4 \
     --initialization-actions gs://dataproc-initialization-actions/python/pip-install.sh \
     --master-machine-type=n1-standard-8 \
-    --max-idle 15m \
+    --max-age=3h \
     --metadata "PIP_PACKAGES=${requirements}" \
     --num-workers=5 \
     --num-preemptible-workers=20 \
@@ -18,4 +18,7 @@ gcloud beta dataproc clusters create jupyter-ensemble-broken-${RANDOM} \
     --project moz-fx-data-bq-srg \
     --properties ^#^spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-latest.jar#spark:spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID}#spark:spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY}#spark:spark.jars.packages=org.apache.spark:spark-avro_2.11:2.4.4#spark:spark.python.profile=true \
     --region us-west1 \
+    --num-master-local-ssds=2 \
+    --num-worker-local-ssds=2  \
+    --num-preemptible-worker-local-ssds=2 \
     --worker-machine-type=n1-standard-8
