077053a46f0fe2ce776e65c8d31f970eea4e18cc
Victor Ng <victor@crankycoder.com>
2020-03-23 12:56:42 -0400

    use 0.4.9 of taar for better logging

--- jupyter/ensemble.sh
+++ jupyter/ensemble.sh
@@ -2,7 +2,7 @@
 
 AWS_ACCESS_KEY_ID=AKIA5PV4VQIY4HJLNP4B
 AWS_SECRET_ACCESS_KEY=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
-requirements="mozilla-taar3==0.4.8 mozilla-srgutil==0.2.1 python-decouple==3.1 click==7.0 boto3==1.7.71 dockerflow==2018.4.0"
+requirements="mozilla-taar3==0.4.9 mozilla-srgutil==0.2.1 python-decouple==3.1 click==7.0 boto3==1.7.71 dockerflow==2018.4.0"
 
 gcloud beta dataproc clusters create jupyter-ensemble-${RANDOM} \
     --bucket jupyter-ensemble \
@@ -10,15 +10,13 @@ gcloud beta dataproc clusters create jupyter-ensemble-${RANDOM} \
     --image-version=1.4 \
     --initialization-actions gs://dataproc-initialization-actions/python/pip-install.sh \
     --master-machine-type=n1-standard-8 \
-    --max-age=3h \
+    --max-age=6h \
     --metadata "PIP_PACKAGES=${requirements}" \
-    --num-workers=5 \
-    --num-preemptible-workers=20 \
+    --num-workers=35 \
     --optional-components=ANACONDA,JUPYTER \
     --project moz-fx-data-bq-srg \
     --properties ^#^spark:spark.jars=gs://spark-lib/bigquery/spark-bigquery-latest.jar#spark:spark.hadoop.fs.s3a.access.key=${AWS_ACCESS_KEY_ID}#spark:spark.hadoop.fs.s3a.secret.key=${AWS_SECRET_ACCESS_KEY}#spark:spark.jars.packages=org.apache.spark:spark-avro_2.11:2.4.4#spark:spark.python.profile=true \
     --region us-west1 \
     --num-master-local-ssds=2 \
     --num-worker-local-ssds=2  \
-    --num-preemptible-worker-local-ssds=2 \
     --worker-machine-type=n1-standard-8
