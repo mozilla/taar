1c0d686f32bab0c884b24b20000fa2c9114180e4
Victor Ng <victor@crankycoder.com>
2020-09-01 11:40:16 -0400

    added a noop fixture loader module for tests

---
+++ tests/noop_fixtures.py
@@ -0,0 +1,41 @@
+"""
+
+Noop helpers
+"""
+
+import mock
+from taar.recommenders.redis_cache import AddonsCoinstallCache
+
+
+def noop_taarlite_dataload(stack):
+    # no-op the taarlite rankdata
+    stack.enter_context(
+        mock.patch.object(AddonsCoinstallCache, "_update_rank_data", return_value=None)
+    )
+    # no-op the taarlite guidguid data
+    stack.enter_context(
+        mock.patch.object(
+            AddonsCoinstallCache, "_update_coinstall_data", return_value=None,
+        )
+    )
+    return stack
+
+
+def noop_taarlocale_dataload(stack):
+    # no-op the taarlite rankdata
+    stack.enter_context(
+        mock.patch.object(
+            AddonsCoinstallCache, "_update_locale_data", return_value=None
+        )
+    )
+    return stack
+
+
+def noop_taarcollab_dataload(stack):
+    # no-op the taar collab
+    stack.enter_context(
+        mock.patch.object(
+            AddonsCoinstallCache, "_update_collab_data", return_value=None
+        )
+    )
+    return stack
